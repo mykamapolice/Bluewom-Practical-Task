7803b006e0627d4af2a191709b2544b27579aaca
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-02-11 19:38:19 +0100

    feat: add first request and table render

--- src/js/components/page.js
+++ src/js/components/page.js
@@ -1,14 +1,24 @@
+const TABLE = {
+  A: 'a',
+  B: 'b',
+  C: 'c',
+};
+
 export default class Page {
   constructor(rootElement) {
     this.rootElement = rootElement;
     this.header = null;
     this.sidebar = null;
     this.pageBody = null;
+    // eslint-disable-next-line prefer-destructuring
+    this.table = TABLE.A;
+    this.currenies = null;
   }
 
-  render() {
+  async render() {
     this.createHeader();
     this.createsidebar();
+    await this.getCurrency();
     this.createTable();
     this.createPageBody();
 
@@ -60,29 +70,78 @@ export default class Page {
     const pageWrapper = document.createElement('section');
     pageWrapper.classList.add('pageBody');
 
+    const buttonsWrapper = document.createElement('div');
+    const btnA = document.createElement('button');
+    const btnB = document.createElement('button');
+    const btnC = document.createElement('button');
+
+    buttonsWrapper.append(btnA, btnB, btnC);
+
+    btnA.innerText = 'tabela A';
+    btnB.innerText = 'tabela B';
+    btnC.innerText = 'tabela C';
+
+    pageWrapper.appendChild(buttonsWrapper);
     pageWrapper.appendChild(this.table);
 
     this.pageBody = pageWrapper;
   }
 
   createTable() {
+    const tableWrapper = document.createElement('div');
     const table = document.createElement('table');
     const trHeader = document.createElement('tr');
-    const thCureency = document.createElement('th');
+    const thCurrency = document.createElement('th');
     const thCode = document.createElement('th');
     const thRate = document.createElement('th');
     const thFollow = document.createElement('th');
 
-    thCureency.innerText = 'Currency';
+    thCurrency.innerText = 'Currency';
     thCode.innerText = 'Code';
     thRate.innerText = 'Middle currency rate';
     thFollow.innerText = 'Follow';
 
+    tableWrapper.classList.add('table-wrapper');
     table.classList.add('pageBody__currency-table');
-
-    trHeader.append(thCureency, thCode, thRate, thFollow);
+    trHeader.append(thCurrency, thCode, thRate, thFollow);
     table.appendChild(trHeader);
 
-    this.table = table;
+    const { rates } = this.currenies[0];
+
+    rates.forEach((item) => {
+      const trCell = document.createElement('tr');
+      const trCurrency = document.createElement('td');
+      const trCode = document.createElement('td');
+      const trRate = document.createElement('td');
+      const trFollow = document.createElement('td');
+      const followBtn = document.createElement('button');
+
+      trCurrency.innerText = item.currency;
+      trCode.innerText = item.code;
+      trRate.innerText = item.mid;
+      followBtn.innerText = 'Follow';
+      trFollow.appendChild(followBtn);
+
+      trCell.append(trCurrency, trCode, trRate, trFollow);
+      console.log(trCell);
+      table.appendChild(trCell);
+    });
+
+    tableWrapper.appendChild(table);
+
+    this.table = tableWrapper;
+  }
+
+  async getCurrency() {
+    try {
+      const response = await fetch('http://api.nbp.pl/api/exchangerates/tables/a/');
+      const currencies = await response.json();
+      currencies.forEach((el) => {
+        console.log(el);
+      });
+      this.currenies = currencies;
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import Page from './components/page';
 
 const page = new Page(document.body);
 page.render();
+page.getCurrency();
